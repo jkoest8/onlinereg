b29ec14e92edb35fb9c79e3da258904ce868ff90
jkoest8 <146107397+jkoest8@users.noreply.github.com>
2023-09-26 10:42:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const SELCO_ZIP_CODES = ["56007", "55912", "55917", "55918", "55921", "55009", "55923", "55927",
+let selcoZipCodes = ["56007", "55912", "55917", "55918", "55921", "55009", "55923", "55927",
         "55021", "55936", "55939", "55941", "55943", "55944", "55946", "55949",
         "55041", "55947", "55951", "55046", "55954", "55057", "55060", "55963",
         "55964", "55965", "55971", "55066", "55972", "55974", "55976", "55975",
         "55981", "55985", "55987", "55992", "55901", "55902", "55903", "55904", "55905", "55906"];
 
-document.getElementById('checkButton').addEventListener('click', function() {
-    const zipInput = document.getElementById('zipInput').value;
-    if (SELCO_ZIP_CODES.includes(zipInput)) {
-        // ZIP code is in SELCO
-        // Generating Barcode Number, replace it with your logic.
-        const barcodeNumber = '1101' + Math.floor(100000000000 + Math.random() * 900000000000);
-        
-        // Generate visual barcode.
-        JsBarcode("#barcode", barcodeNumber, {
-            format: "CODE128",
-            lineColor: "#000",
-            width: 2,
-            height: 40,
-            displayValue: true,
-        });
-        
+function checkZipCode() {
+    let zipCode = document.getElementById('zipCode').value;
+    let resultDiv = document.getElementById('result');
+
+    if (selcoZipCodes.includes(zipCode)) {
+        resultDiv.textContent = "ZIP Code is in SELCO. Generating Barcode...";
+        let barcode = generateUniqueBarcode();
+        resultDiv.textContent += `Generated Barcode: ${barcode}`;
     } else {
-        // ZIP code is not in SELCO
-        // Display a message or take any other action.
-        alert("ZIP code is not in SELCO");
+        resultDiv.textContent = "ZIP Code is not in SELCO.";
     }
-});
+}
+
+let generatedCodes = [];
+
+function generateUniqueBarcode() {
+    let prefix = "1101";
+    let newCode;
+
+    do {
+        newCode = prefix + Math.floor(100000000 + Math.random() * 900000000);
+    } while (generatedCodes.includes(newCode));
+
+    generatedCodes.push(newCode);
+    return newCode;
+}
