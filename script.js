a55f32cff644eb89e692786e36ac912e938e23f4
jkoest8 <146107397+jkoest8@users.noreply.github.com>
2023-09-26 10:47:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let selcoZipCodes = ["56007", "55912", "55917", "55918", "55921", "55009", "55923", "55927",
-        "55021", "55936", "55939", "55941", "55943", "55944", "55946", "55949",
-        "55041", "55947", "55951", "55046", "55954", "55057", "55060", "55963",
-        "55964", "55965", "55971", "55066", "55972", "55974", "55976", "55975",
-        "55981", "55985", "55987", "55992", "55901", "55902", "55903", "55904", "55905", "55906"];
+    "55021", "55936", "55939", "55941", "55943", "55944", "55946", "55949",
+    "55041", "55947", "55951", "55046", "55954", "55057", "55060", "55963",
+    "55964", "55965", "55971", "55066", "55972", "55974", "55976", "55975",
+    "55981", "55985", "55987", "55992", "55901", "55902", "55903", "55904", "55905", "55906"];
+
+let generatedCodes = [];
 
 function checkZipCode() {
     let zipCode = document.getElementById('zipCode').value;
@@ -12,12 +14,19 @@ function checkZipCode() {
         resultDiv.textContent = "ZIP Code is in SELCO. Generating Barcode...";
         let barcode = generateUniqueBarcode();
         resultDiv.textContent += `Generated Barcode: ${barcode}`;
+        
+        // Generate the visual barcode here.
+        JsBarcode("#barcode", barcode, {
+            format: "CODE128",
+            lineColor: "#000",
+            width: 2,
+            height: 40,
+            displayValue: true,
+        });
     } else {
         resultDiv.textContent = "ZIP Code is not in SELCO.";
     }
 }
-
-let generatedCodes = [];
 
 function generateUniqueBarcode() {
     let prefix = "1101";
@@ -30,11 +39,3 @@ function generateUniqueBarcode() {
     generatedCodes.push(newCode);
     return newCode;
 }
-
-JsBarcode("#barcode", newCode, {
-  format: "CODE128",
-  lineColor: "#000",
-  width: 2,
-  height: 40,
-  displayValue: true,
-});
